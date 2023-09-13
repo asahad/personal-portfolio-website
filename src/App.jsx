83a71f5cd83330001309219b26d14df1719ab885
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2023-09-13 14:27:36 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import firebase from "./FirebaseConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <div>
+          <h1>Personal portolio</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
