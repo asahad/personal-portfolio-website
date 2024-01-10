c94d2c8a0b380d50902d21e468f70e68dedeaf98
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2024-01-10 14:25:02 -0600

    Updated Abot Me Section

--- backend/formSubmissionRoute.js
+++ backend/formSubmissionRoute.js
@@ -3,4 +3,10 @@ const router = express.Router();
 import { sendForm, trialEndpoint} from "./formSubmissioncontroller.js";
 router.route("/form-submission").post(sendForm);
 router.route("/trial").get(trialEndpoint)
+
+
+
+
+
+
 export default router;
