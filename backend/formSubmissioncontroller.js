e2983ee30f451cec8beab8513e470c66ef9617a1
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2024-01-10 00:45:32 -0600

    reverting to commit e2aa90c

--- backend/formSubmissioncontroller.js
+++ backend/formSubmissioncontroller.js
@@ -1,23 +1,13 @@
-import { sendFormSubmissionEmail } from './utils.js'; 
 // Form Submission Controllers
 // Handle Form Subsmission
 // route-POST/form-submission
 // access-public
 
 const sendForm = async (req, res) => {
-  const { name, email } = req.body;
-  try {
-    // Send confirmation email
-    sendFormSubmissionEmail(email, name);
-
-    res.status(200).json({
-      message: "Form submitted Successfully",
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Error processing form submission",
-    });
-  }
+  const formData = req.body;
+  res.status(200).json({
+    message: "Form submitted Successfully",
+  });
 };
 
 
--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,53 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleInputChange = (event) => {
-    const { id, value } = event.target;
-    setFormData({ ...formData, [id]: value });
-  };
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    try {
-      const response = await fetch(
-        "https://personal-portfolio-backend-s3en.onrender.com/api/form-submission",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
-
-      if (response.ok) {
-        toast.success("Form submitted successfully!");
-        // Optional: Reset form here
-        setFormData({ name: "", email: "", message: "" });
-      } else {
-        toast.error("Failed to submit form.");
-      }
-    } catch (error) {
-      toast.error("An error occurred while submitting the form.");
-    }
+    // Handle form submission logic here
   };
 
   return (
     <div className="contact-section-background" id="contact">
-      <ToastContainer />
-      <Container className="pt-5">
+      <Container className="pt-5 ">
         <Row className="justify-content-center">
           <Col md={6}>
+            {" "}
+            {/* Set the horizontal length of the form */}
             <h1
               className="text-center"
               style={{
@@ -61,24 +28,12 @@ const ContactForm = () => {
             <Form onSubmit={handleSubmit} className="m-2">
               <Form.Group className="mb-3" controlId="formName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter your name"
-                  id="name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                />
+                <Form.Control type="text" placeholder="Enter your name" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter your email"
-                  id="email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                />
+                <Form.Control type="email" placeholder="Enter your email" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formMessage">
@@ -87,13 +42,12 @@ const ContactForm = () => {
                   as="textarea"
                   rows={3}
                   placeholder="Your message..."
-                  id="message"
-                  value={formData.message}
-                  onChange={handleInputChange}
                 />
               </Form.Group>
 
               <div className="text-center">
+                {" "}
+                {/* Center the button */}
                 <Button variant="primary" type="submit">
                   Submit
                 </Button>
