1890f0bc2d600cff70369f9a26599266085c5324
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2024-01-10 20:45:08 -0600

    update readme

--- backend/formSubmissionRoute.js
+++ backend/formSubmissionRoute.js
@@ -3,10 +3,4 @@ const router = express.Router();
 import { sendForm, trialEndpoint} from "./formSubmissioncontroller.js";
 router.route("/form-submission").post(sendForm);
 router.route("/trial").get(trialEndpoint)
-
-
-
-
-
-
 export default router;
