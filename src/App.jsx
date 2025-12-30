41abba4e845f2c09353ef90b40139c6fbb49a6ae
asahad <alhassansahad24@gmail.com>
2025-12-29 21:53:29 -0800

    remove firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Button } from "react-bootstrap";
-import firebase from "./FirebaseConfig";
 import Header from "./components/Header";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
