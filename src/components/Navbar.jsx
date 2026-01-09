bdf3cf16bc36f2398fac8f2f658d70e656e8a106
asahad <alhassansahad24@gmail.com>
2026-01-09 10:21:27 -0800

    refactored

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
+
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 const MyNavbar = () => {
   return (
-    <Navbar bg="white" expand="lg" fixed="top" className="shadow-sm">
+    <Navbar expand="lg" fixed="top" className="custom-navbar shadow-sm">
       <Container>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
+        <Navbar.Brand href="#top" className="navbar-brand-text">
+          Alhassan Sahad
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
           <Nav className="ms-auto">
-            <Nav.Link href="#top">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#top" className="nav-link-custom">Home</Nav.Link>
+            <Nav.Link href="#skills" className="nav-link-custom">Skills</Nav.Link>
+            <Nav.Link href="#projects" className="nav-link-custom">Projects</Nav.Link>
+            <Nav.Link href="#contact" className="nav-link-custom">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
